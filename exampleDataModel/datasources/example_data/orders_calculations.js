89eb722be2ed9ad3016bc1ea2aab5ca856584f09
Sean <sdevlin@servoy.com>
2017-02-21 22:56:16 -0500

    Display address calc on orders

--- exampleDataModel/datasources/example_data/orders_calculations.js
+++ exampleDataModel/datasources/example_data/orders_calculations.js
@@ -1,4 +1,16 @@
 /**
+ * @properties={type:12,typeid:36,uuid:"8D28CA37-8150-4AF6-8B6E-3A004F856C3B"}
+ */
+function displayAddress()
+{
+	return [
+		shipaddress,
+		shipcity,
+		shipcountry + ' ' + shippostalcode
+	].join('\n')
+}
+
+/**
  * Returns the calculated total of the order all items + freight
  * @properties={type:8,typeid:36,uuid:"D4BA139C-3E72-48D1-BDD4-ADA10D44F3BE"}
  */
