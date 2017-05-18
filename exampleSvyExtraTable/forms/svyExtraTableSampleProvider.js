5e67093f19ff2dd7a609ed7f8508797fcc97952f
paronne <paronne@servoy.com>
2017-05-18 18:06:23 +0200

    svyExtraTable sample

---
+++ exampleSvyExtraTable/forms/svyExtraTableSampleProvider.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * @return {String}
+ *
+ * @properties={typeid:24,uuid:"885B9315-8757-4374-86AB-B6E656B9AC7A"}
+ */
+function getName() {
+	return "1. Servoy Extra Table";
+}
+
+/**
+*
+* @return {String}
+*
+* @properties={typeid:24,uuid:"ECD07DCD-CD8A-4721-AF4F-6D87D86FD81F"}
+*/
+function getDescription() {
+	return "Fast read only grid";
+}
+
+/**
+*
+* @return {String}
+*
+* @properties={typeid:24,uuid:"772DD581-9FFF-491B-AFB6-F04AB25C8277"}
+*/
+function getIconStyleClass() {
+	return "fa fa-list-alt"
+}
