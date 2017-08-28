11da71673e243a13f1d5a3db5edf204687485da1
paronne <paronne@servoy.com>
2017-08-28 13:25:07 +0300

    Move exampleCSS and exampleDataModel

--- exampleCSS/forms/cssSampleProvider.frm
+++
@@ -1,5 +0,0 @@
-customProperties:"formComponent:false",
-extendsID:"1046CC5D-E653-4F9C-B8B9-E3FCDA83A211",
-name:"cssSampleProvider",
-typeid:3,
-uuid:"A78CF677-5481-4AEB-A08A-E2271DABA71B"
--- exampleDataModel/datasources/example_data/order_details_calculations.js
+++
@@ -1,8 +0,0 @@
-/**
- * @properties={type:8,typeid:36,uuid:"4120E900-D472-4428-BC2F-F1E93831A05D"}
- */
-function subtotal()
-{
-	var amt = quantity * unitprice * (1-discount);
-	return parseFloat(amt.toFixed(2));
-}
